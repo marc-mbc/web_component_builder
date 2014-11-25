025912f22a4e4e9cca32105507511bf0ccd212d1
Marc Blando Coll <marc.blando@engrande.com>
2014-11-25 10:18:07 +0100

    fix module name

--- usage_example.js
+++ usage_example.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var web_component_name = require('path').basename(__dirname);
-var tools = require('./tool.js')({
+var tools = require('bdgt-web-component-builder')({
   name: web_component_name,
   root: '../../../public/assets',
   subfolder: 'web_component_dist/' + web_component_name
