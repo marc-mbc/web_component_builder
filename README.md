097ff5fd61e0c3e67051df0d62ad128e2f59076a
Marc Blando Coll <marc.mbc@gmail.com>
2014-11-30 01:39:12 +0100

    Added some improvements and compatibility with dependences

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
 ## Budgetplaces Web component builder
 
-This module exports a common set of tools and gulp tasks to build a web_component. You can use it for example:
-
-'use strict';
-
-var wc_name = require('path').basename(__dirname);
-var tools = require('./tool.js')({
-  name: wc_name,
-  root: '../../../public/assets',
-  subfolder: 'wc_dist/' + wc_name
-});
-// Add any extra tasks
-tools.gulp.task('default', ['watch']);
+This module exports a common set of tools and gulp tasks to build a web_component. Check usage_example.js to see how to use it
 
     
--- usage_example.js
+++ usage_example.js
@@ -1,10 +1,18 @@
 'use strict';
-
-var web_component_name = require('path').basename(__dirname);
-var tools = require('bdgt-web-component-builder')({
-  name: web_component_name,
-  root: '../../../public/assets',
-  subfolder: 'web_component_dist/' + web_component_name
+var wc_name = require('path').basename(__dirname);
+var tools = require('./component_build.js')({
+  wc_name: wc_name,
+  root: __dirname,
+  main_root: __dirname
 });
 
-tools.gulp.task('default', ['watch']);
+// tools has gulp, many tools and all web component tasks (with sufix _{{Web Component Name}}) 
+// for this web component and all of his dependences
+// Here you can add more tasks if you need
+
+
+// In this example we want as a default task a watch of the main component. tools.all_dependences is a set
+// of tasks that we need to move all images and html of every dependence to the public folder 
+tools.gulp.task('default', ['watch_' + wc_name].concat(Object.keys(tools.all_dependences)), function () {
+  tools.connect().use(tools.serveStatic(__dirname + '/public')).listen(8080);
+});
